47d1031743531b271c857260b9b8eac9b8804b9f
Huabiao Xu <huabiao.xu@gmail.com>
2015-09-29 23:21:37 -0400

    added mkdir to setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 hostname=$(hostname)
 
+ln -s common ~/dotfiles/${hostname}
+
 mkdir ~/.dotfiles/zsh
 ln -s ~/dotfiles/${hostname}/profile ~/.dotfiles/profile
 ln -s ~/dotfiles/common/tmux ~/.dotfiles/tmux
@@ -16,7 +18,7 @@ ln -s .dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s .dotfiles/vim/vimrc ~/.vimrc
 ln -s .dotfiles/zsh/zshrc ~/.zshrc
 
-mkdir ~/.hist
+mkdir -p ~/.hist/vim_swp
 
 pushd ~/dotfiles/common/vim
 git clone https://github.com/Shougo/neobundle.vim.git bundle/neobundle.vim
