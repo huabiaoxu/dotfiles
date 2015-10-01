5fc922bc2c7fd1005fce4dd42427f242ea923833
Huabiao Xu <hx372@linax1.cims.nyu.edu>
2015-10-01 00:17:46 -0400

    bash in setup

--- setup.sh
+++ setup.sh
@@ -1,22 +1,34 @@
 #/usr/bin/env zsh
 
-hostname=$(hostname)
+PROFILENAME=${PROFILENAME:=`hostname -s`}
 
-ln -s common ~/dotfiles/${hostname}
+ln -s common ~/dotfiles/${PROFILENAME}
 
 mkdir ~/.dotfiles/zsh
-ln -s ~/dotfiles/${hostname}/profile ~/.dotfiles/profile
+mkdir ~/.dotfiles/bash
+ln -s ~/dotfiles/${PROFILENAME}/profile ~/.dotfiles/profile
 ln -s ~/dotfiles/common/tmux ~/.dotfiles/tmux
 ln -s ~/dotfiles/common/vim ~/.dotfiles/vim
 
-echo "export DOTFILES_DIR=~/dotfiles/${hostname}
+echo "export DOTFILES_DIR=~/dotfiles/${PROFILENAME}
 
 . \${DOTFILES_DIR}/zsh/zshrc" > ~/.dotfiles/zsh/zshrc
+
+
+echo "export DOTFILES_DIR=~/dotfiles/${PROFILENAME}
+
+. \${DOTFILES_DIR}/bash/bashrc" > ~/.dotfiles/bash/bashrc
+
+echo "export DOTFILES_DIR=~/dotfiles/${PROFILENAME}
+
+. \${DOTFILES_DIR}/bash/bash_profile" > ~/.dotfiles/bash/bash_profile
 
 ln -s .dotfiles/profile/profile ~/.profile
 ln -s .dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s .dotfiles/vim/vimrc ~/.vimrc
 ln -s .dotfiles/zsh/zshrc ~/.zshrc
+ln -s .dotfiles/bash/bashrc ~/.bashrc
+ln -s .dotfiles/bash/bash_profile ~/.bash_profile
 
 mkdir -p ~/.hist/vim_swp
 
